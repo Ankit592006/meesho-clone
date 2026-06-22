af03bd8f00e5792e09d9b20474d3a6cc53c11fbd
Ankit <ankitchaudri123@gmail.com>
2026-06-22 16:02:12 +0530

    Responsive Css Added into some parts

--- js/app.js
+++ js/app.js
@@ -1,20 +1,4 @@
-// ===========================================
-// APP ENTRY POINT
-// ===========================================
 
-// $(document).ready(function () {
-
-//     console.log("Meesho Clone Started");
-
-// });
-// $("#productsSection").html(productsSection());
-
-// $("#filtersPanelContainer").html(filtersPanel());
-
-// $("#sortDropdownContainer").html(sortDropdown());
-
-// // $("#productGridContainer").html(productGrid());
-// $("#filtersContainer").html(buildFilters());
 $(document).ready(function () {
 
     console.log("Meesho Clone Started");
